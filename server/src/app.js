34b2682a309280a14d747811f5ca1fff6c473a2b
Abby Rictor <amrictor@gmail.com>
2021-02-06 22:29:29 -0700

    fix https

--- server/src/app.js
+++ server/src/app.js
@@ -10,14 +10,14 @@ app.use(bodyparser.json());
 const publicAPI = require('./api')
 app.use('/api/public', cors(), publicAPI);
 
-const credentials = process.env.ENV === 'PROD' ? {
+const credentials = {
   key: fs.readFileSync('ssl.key'), 
   cert: fs.readFileSync('ssl.crt')
-} : {};
+};
 const httpsServer = https.createServer(credentials, app);
 
 const port = process.env.PORT || 8080;
 
-(process.env.ENV === 'PROD' ? httpsServer : app).listen(port, () => console.log(`Listening on port ${port}..`));
+httpsServer.listen(port, () => console.log(`Listening on port ${port}..`));
 
 
