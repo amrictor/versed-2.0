77ad097b20765a6f5f540a9dab359a919af25f84
BuildTools <amrictor@gmail.com>
2021-02-06 22:09:10 -0700

    ssl fixes for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const https = require('https');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
+
+const app = express();
+app.use(bodyparser.json());
+const publicAPI = require('./api')
+app.use('/api/public', cors(), publicAPI);
 
 const credentials = process.env.ENV === 'PROD' ? {
   key: fs.readFileSync('ssl.key'), 
   cert: fs.readFileSync('ssl.crt')
 } : {};
-const app = express(credentials);
-app.use(bodyparser.json());
+const httpsServer = https.createServer(credentials, app);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
 
-const publicAPI = require('./api')
+(process.env.ENV === 'PROD' ? httpsServer : app).listen(port, () => console.log(`Listening on port ${port}..`));
 
-app.use('/api/public', cors(), publicAPI);
+
